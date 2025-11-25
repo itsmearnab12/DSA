eb45444c62599292cd5321e6f900737ae4a65500
Arnab <arnabchoudhury2005@gmail.com>
2025-11-25 18:28:11 +0530

    Problem2 and 3

---
+++ Java_problems/Problem2.java
@@ -0,0 +1,9 @@
+class Problem2 {
+public static void main(String[] args) {
+    int i = 5;
+    for (i = 0; i < 5; i++) {
+        System.out.print("*");
+    }
+}
+     
+}
---
+++ Java_problems/Problem3.java
@@ -0,0 +1,13 @@
+public class Problem3 {
+    public static void main(String[] args) {
+        int n = 5;
+        int nst = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*");
+            }
+            nst++;
+            System.out.println();
+        }
+    }
+}
