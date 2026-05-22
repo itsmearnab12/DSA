3d02fd096dd26164f083a1e9873a3458c61891c6
Arnab <arnabchoudhury2005@gmail.com>
2026-05-22 08:44:14 +0530

    Solved LC 3043

---
+++ Leetcode/Problem3043.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Problem3043 {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+
+        HashSet<String> set = new HashSet<>();
+
+        // Store all prefixes of arr1
+        for(int num : arr1) {
+
+            String s = String.valueOf(num);
+
+            String prefix = "";
+
+            for(int i = 0; i < s.length(); i++) {
+
+                prefix += s.charAt(i);
+
+                set.add(prefix);
+            }
+        }
+
+        int max = 0;
+
+        // Check prefixes of arr2
+        for(int num : arr2) {
+
+            String s = String.valueOf(num);
+
+            String prefix = "";
+
+            for(int i = 0; i < s.length(); i++) {
+
+                prefix += s.charAt(i);
+
+                if(set.contains(prefix)) {
+
+                    max = Math.max(max, prefix.length());
+                }
+            }
+        }
+
+        return max;
+    }
+}
