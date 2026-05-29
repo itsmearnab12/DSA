a1972487fe65768f8a288a69d5f2ff23868ad4fa
Arnab <arnabchoudhury2005@gmail.com>
2026-05-29 06:57:20 +0530

    Solved LC 3300

---
+++ Leetcode/Problme3300.java
@@ -0,0 +1,18 @@
+class Problem3300 {
+    public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            int n = nums[i];
+            int sum = 0;
+            while(n>0){
+                int digit = n%10;
+                sum = sum + digit;
+                n = n/10;
+            }
+            if(sum<min){
+                min = sum;
+            }
+        }
+        return min;
+    }
+}
