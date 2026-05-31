a362fd8caae867b044cd20352d200a5527f326a2
Arnab <arnabchoudhury2005@gmail.com>
2026-05-31 07:47:16 +0530

    Solved LC 2126 but need to understand more

---
+++ Leetcode/Problem2126.java
@@ -0,0 +1,14 @@
+class Problem2126 {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long currentMass = mass;
+        for(int i = 0; i < asteroids.length; i++){
+            if(currentMass < asteroids[i]){
+                return false;
+                
+            }
+            currentMass += asteroids[i];
+        }
+        return true;
+    }
+}
